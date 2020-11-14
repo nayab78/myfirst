897dc808bcff282f6fc7fa32bd430d7f7b35b110
nayab shaik <nayabshaik741@gmail.com>
2020-11-14 07:39:00 +0530

    project

---
+++ src/main/java/icicibank/serialization/XmlSerializationProcessor.java
@@ -0,0 +1,16 @@
+package icicibank.serialization;
+
+import org.restexpress.serialization.xml.XstreamXmlProcessor;
+
+public class XmlSerializationProcessor
+extends XstreamXmlProcessor
+{
+	public XmlSerializationProcessor()
+    {
+	    super();
+//		alias("element_name", Element.class);
+//		alias("element_name", Element.class);
+//		alias("element_name", Element.class);
+//		alias("element_name", Element.class);
+    }
+}
